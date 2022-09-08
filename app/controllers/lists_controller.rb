74668c4aae4b51a84521dbc982a4fd2b4b9f73c5
Alyssa Falcione <lyzarddz@gmail.com>
2022-09-08 15:20:11 -0400

    added post to list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,4 +5,14 @@ class ListsController < ApplicationController
         list.to_json
     end
 
+    post "/lists" do
+        @list  = List.new(params[:list])
+ 
+         if list.save
+             @list.to_json
+         else
+             {errors: @list.errors.full_messages}.to_json
+         end
+     end
+
 end
